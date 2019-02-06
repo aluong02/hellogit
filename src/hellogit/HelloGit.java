d5c602551eae4ad97450a54bbef25088b299cf58
amlpm8 <amlpm8@ES00336.stl.umsl.edu>
2019-02-05 18:15:00 -0600

    added hello git print out

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -15,7 +15,9 @@ public class HelloGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+  System.out.println("Hello, Git");
+      
+  
     }
     
 }
